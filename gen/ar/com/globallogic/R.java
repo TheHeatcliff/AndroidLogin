de806042c18463c41c6d90dd997628ecd42771a3
Nikolas Gonzalez <nicolas.hernan.gonzalez@gmail.com>
2013-03-22 00:10:15 -0300

    un tach mas presentable

--- gen/ar/com/globallogic/R.java
+++ gen/ar/com/globallogic/R.java
@@ -26,12 +26,12 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080006;
-        public static final int btn_login=0x7f080004;
-        public static final int div_error=0x7f080005;
-        public static final int et_pw=0x7f080003;
-        public static final int et_un=0x7f080001;
-        public static final int tv_pw=0x7f080002;
-        public static final int tv_un=0x7f080000;
+        public static final int buton_login=0x7f080004;
+        public static final int div_response=0x7f080005;
+        public static final int editText_password=0x7f080003;
+        public static final int editText_userName=0x7f080001;
+        public static final int textView_password=0x7f080002;
+        public static final int textView_userName=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
--- src/ar/com/globallogic/LoginActivity.java
+++ src/ar/com/globallogic/LoginActivity.java
@@ -1,17 +1,38 @@
 package ar.com.globallogic;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
-    @Override
+	 private EditText userName;
+	 
+	 private EditText password;
+	 
+	 private TextView reponse;
+	 
+	 private Button loginButton;
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        
+		super.onCreate(savedInstanceState);
+        
+		setContentView(R.layout.activity_login);
+        
+		userName = (EditText) findViewById(R.id.editText_userName);
+        password = (EditText) findViewById(R.id.editText_password);
+        reponse = (TextView) findViewById(R.id.div_response);
+        loginButton = (Button) findViewById(R.id.buton_login);
+        
+        loginButton.setOnClickListener(new LoginListener());
+		
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,4 +41,18 @@ public class LoginActivity extends Activity {
         return true;
     }
     
+    
+    
+    private class LoginListener implements View.OnClickListener {
+    	
+    	public void onClick(View v) {
+    		
+    		if ((!userName.getText().toString().equals("niko")) && (!password.getText().toString().equals("1234"))){
+    			reponse.setText("Invalid Login!");
+    		}else {
+    			reponse.setText("logged in");
+    		}
+    	}
+    	
+    }
 }
